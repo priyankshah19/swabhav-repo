bae5870ca34bb4dacc8f09af0cc6def5cf42c2ad
priyank <shahpriyank50@gmail.com>
2019-04-13 16:57:02 +0530

    ignore

--- OPP/BasicApp/BasicApp/Program.cs
+++ OPP/BasicApp/BasicApp/Program.cs
@@ -6,6 +6,12 @@ namespace BasicApp
         static void Main(string[] args)
         {
             Console.WriteLine("hello");
+            Console.WriteLine (true);
+            Console.WriteLine('p');
+            Console.WriteLine(3.5);
+            Console.WriteLine(314323529321120);
+
+
         }
     }
 }
---
+++ OPP/OverloadingApps/OverloadingApps/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadingApps
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PrintThis(4);
+            PrintThis('e');
+            PrintThis(true);
+            PrintThis(3.5);
+            PrintThis("hello");
+        }
+        public static void PrintThis(int j)
+        {
+            Console.WriteLine("overload1" + j);
+
+        }
+        public static void PrintThis(char i)
+        {
+            Console.WriteLine("overload2" + i);
+
+
+        }
+
+        public static void PrintThis(Boolean x)
+        {
+            Console.WriteLine("overload3" + x);
+
+
+        }
+
+        public static void PrintThis(double y)
+        {
+            Console.WriteLine("overload4" + y);
+
+
+        }
+        public static void PrintThis(string s)
+        {
+            Console.WriteLine("overload5" + s);
+
+
+        }
+
+
+    }
+}
+
+    
