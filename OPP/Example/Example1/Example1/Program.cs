824419167b2d82c562bc1fe475733bb6375a179f
priyank <shahpriyank50@gmail.com>
2019-04-20 08:31:48 +0530

    example

---
+++ OPP/Example/Example1/Example1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example1
+{
+    
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+                PrintThis(4);
+                PrintThis('e');
+                PrintThis(true);
+                PrintThis(3.5);
+                PrintThis("hello");
+            }
+            public static void PrintThis(int j)
+            {
+                Console.WriteLine("overload1" + j);
+
+            }
+            public static void PrintThis(char i)
+            {
+                Console.WriteLine("overload2" + i);
+
+
+            }
+
+            public static void PrintThis(Boolean x)
+            {
+                Console.WriteLine("overload3" + x);
+
+
+            }
+
+            public static void PrintThis(double y)
+            {
+                Console.WriteLine("overload4" + y);
+
+
+            }
+            public static void PrintThis(string s)
+            {
+                Console.WriteLine("overload6" + s);
+
+
+            }
+
+
+        }
+    }
+
+
+
